b8454eb660f4e879fa6001fc9d9fb469525540a5
Oleg Nenashev <nenashev@synopsys.com>
2013-08-25 22:28:46 +0400

    Nested appearance of job restrictions
    Signed-off-by: Oleg Nenashev <nenashev@synopsys.com>

--- src/main/java/com/synopsys/arc/jenkinsci/plugins/jobrestrictions/jobs/JobRestrictionPropertyConfig.java
+++ src/main/java/com/synopsys/arc/jenkinsci/plugins/jobrestrictions/jobs/JobRestrictionPropertyConfig.java
@@ -23,67 +23,50 @@
  */
 package com.synopsys.arc.jenkinsci.plugins.jobrestrictions.jobs;
 
-import com.synopsys.arc.jenkinsci.plugins.jobrestrictions.Messages;
 import hudson.AbortException;
 import hudson.Extension;
-import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.model.Cause;
-import hudson.model.Job;
-import hudson.model.JobProperty;
-import hudson.model.JobPropertyDescriptor;
+import hudson.model.Describable;
+import hudson.model.Descriptor;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  *
  * @author Oleg Nenashev <nenashev@synopsys.com>, Synopsys Inc.
  */
-public class JobRestrictionProperty extends JobProperty {
-    
-    JobRestrictionPropertyConfig config;
+public class JobRestrictionPropertyConfig implements Describable<JobRestrictionPropertyConfig> {
+    UpstreamCauseRestriction upstreamCauseRestriction;
 
     @DataBoundConstructor
-    public JobRestrictionProperty(JobRestrictionPropertyConfig config) {
-        this.config = config;
+    public JobRestrictionPropertyConfig(UpstreamCauseRestriction upstreamCauseRestriction) {
+        this.upstreamCauseRestriction = upstreamCauseRestriction;
     }
 
-    public JobRestrictionPropertyConfig getConfig() {
-        return config;
+    public UpstreamCauseRestriction getUpstreamCauseRestriction() {
+        return upstreamCauseRestriction;
     }
     
-    @Override
-    public boolean prebuild(AbstractBuild build, BuildListener listener) {
-        // Consider build as valid if any cause is valid
-        for ( Object cause : build.getCauses() ) {
-           try {
-            validateCause((Cause)cause, listener);
-           } catch (AbortException ex) {
-               //TODO: Throw Aborted upstairs in the future
-               listener.fatalError("[Job Restrictions] - Build will be aborted: "+ex.getMessage());
-               return false;
-           }
-        }
-
-        // Build is valid
-        return true;
-    }
-    
-    private void validateCause(Cause cause, BuildListener listener) throws AbortException { 
-       if (config != null) {
-           config.validateCause(cause, listener);
-       }
-    }
+    public void validateCause(Cause cause, BuildListener listener) throws AbortException { 
+       if (upstreamCauseRestriction != null && cause instanceof Cause.UpstreamCause) {
+           upstreamCauseRestriction.validate((Cause.UpstreamCause)cause);
+       }    
+       //TODO: checks
        
+    }
+
+    @Override
+    public Descriptor<JobRestrictionPropertyConfig> getDescriptor() {
+        return DESCRIPTOR;
+    }
+    
     @Extension
-    public static class DescriptorImpl extends JobPropertyDescriptor {
+    public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+    public static class DescriptorImpl extends Descriptor<JobRestrictionPropertyConfig> {
+
         @Override
         public String getDisplayName() {
-            return Messages.jobs_JobRestrictionProperty_DisplayName();
-        }
-
-        @Override
-        public boolean isApplicable(Class<? extends Job> jobType) {
-            return true;
+            return "Job restriction property config";
         }
         
     }
