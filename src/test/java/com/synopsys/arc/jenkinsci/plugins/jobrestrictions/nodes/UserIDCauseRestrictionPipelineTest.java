8b1c03de0c3240acaba62666658d96c54163d172
Mark Waite <mark.earl.waite@gmail.com>
2023-06-01 18:35:34 -0600

    Format new test UserIDCauseRestrictionPipelineTest

--- src/test/java/com/synopsys/arc/jenkinsci/plugins/jobrestrictions/nodes/UserIDCauseRestrictionPipelineTest.java
+++ src/test/java/com/synopsys/arc/jenkinsci/plugins/jobrestrictions/nodes/UserIDCauseRestrictionPipelineTest.java
@@ -6,12 +6,20 @@ import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.synopsys.arc.jenkinsci.plugins.jobrestrictions.restrictions.job.StartedByUserRestriction;
+import com.synopsys.arc.jenkinsci.plugins.jobrestrictions.util.UserSelector;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import hudson.model.Cause;
+import hudson.model.CauseAction;
+import hudson.model.Queue;
+import hudson.model.Result;
+import hudson.model.User;
+import hudson.model.queue.QueueTaskFuture;
+import hudson.security.ACL;
+import hudson.slaves.DumbSlave;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-
-import hudson.model.Queue;
-import hudson.model.queue.QueueTaskFuture;
 import org.acegisecurity.context.SecurityContext;
 import org.acegisecurity.context.SecurityContextHolder;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
@@ -24,87 +32,76 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-import com.synopsys.arc.jenkinsci.plugins.jobrestrictions.restrictions.job.StartedByUserRestriction;
-import com.synopsys.arc.jenkinsci.plugins.jobrestrictions.util.UserSelector;
-import com.synopsys.arc.jenkinsci.plugins.jobrestrictions.nodes.JobRestrictionProperty;
-
-import hudson.model.Cause;
-import hudson.model.CauseAction;
-import hudson.model.Result;
-import hudson.model.User;
-import hudson.security.ACL;
-import hudson.slaves.DumbSlave;
-
-import javax.annotation.Nonnull;
 
 public class UserIDCauseRestrictionPipelineTest {
-  
+
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
     private static final String TEST_USERNAME = "foo";
     private static final String TEST_USERNAME_2 = "bar";
-  
+
     @Before
     public void setupSecurityRealm() {
         j.jenkins.setSecurityRealm(j.createDummySecurityRealm());
     }
-  
+
     @Test
     public void nodeShouldAllowPipelineRunsByAcceptedUser_UserIdCause() throws Exception {
         DumbSlave slave = j.createOnlineSlave();
         List<UserSelector> listUserSelector = new ArrayList<UserSelector>(1);
         listUserSelector.add(new UserSelector(TEST_USERNAME));
-      
+
         // add StartedByUser Restriction property to node
-        slave.getNodeProperties().add(new JobRestrictionProperty(new StartedByUserRestriction(listUserSelector, true, true, false)));
-      
+        slave.getNodeProperties()
+                .add(new JobRestrictionProperty(new StartedByUserRestriction(listUserSelector, true, true, false)));
+
         // create pipeline job that will run on the test slave
         final WorkflowJob project = j.jenkins.createProject(WorkflowJob.class, "pipeline_demo");
         project.setDefinition(new CpsFlowDefinition(
-                "node('" + slave.getNodeName() + "') {\n" +
-                "    sh('echo hello')\n" +
-                "}", true));
-      
+                "node('" + slave.getNodeName() + "') {\n" + "    sh('echo hello')\n" + "}", true));
+
         // schedule a build for the pipeline job as the user who is accepted by the test slave
         WorkflowRun run = runAsUserAndWaitForCompletion(project, TEST_USERNAME);
         assertThat("Job restriction should allow pipeline jobs", run.getResult(), equalTo(Result.SUCCESS));
     }
-    
+
     @Test
     public void nodeShouldNotAllowPipelineRunsByUnacceptedUser_UserIdCause() throws Exception {
         DumbSlave slave = j.createOnlineSlave();
         List<UserSelector> listUserSelector = new ArrayList<UserSelector>(1);
         listUserSelector.add(new UserSelector(TEST_USERNAME_2));
-      
+
         // add StartedByUser Restriction property to node
-        slave.getNodeProperties().add(new JobRestrictionProperty(new StartedByUserRestriction(listUserSelector, true, true, false)));
-      
+        slave.getNodeProperties()
+                .add(new JobRestrictionProperty(new StartedByUserRestriction(listUserSelector, true, true, false)));
+
         final WorkflowJob project = j.jenkins.createProject(WorkflowJob.class, "pipeline_demo");
         project.setDefinition(new CpsFlowDefinition(
-                "node('" + slave.getNodeName() + "') {\n" +
-                "    sh('echo hello')\n" +
-                "}", true));
-      
+                "node('" + slave.getNodeName() + "') {\n" + "    sh('echo hello')\n" + "}", true));
+
         // schedule a build for the pipeline job as the user who is not accepted by the test slave
         QueueTaskFuture<WorkflowRun> runFuture = runAsUser(project, TEST_USERNAME);
         WorkflowRun run = runFuture.waitForStart();
         Thread.sleep(2000); // Any way to do it better
-        List<StepExecution> executions = run.getExecutionPromise().get().getCurrentExecutions(true).get();
+        List<StepExecution> executions =
+                run.getExecutionPromise().get().getCurrentExecutions(true).get();
         assertThat("Missing current head", executions.size(), equalTo(1));
         StepExecution currentExecution = executions.get(0);
         assertThat(currentExecution, instanceOf(ExecutorStepExecution.class));
-        ExecutorStepExecution exec = ((ExecutorStepExecution)currentExecution);
+        ExecutorStepExecution exec = ((ExecutorStepExecution) currentExecution);
 
         // Now we know there is a waiting execution
         List<Queue.BuildableItem> buildableItems = j.jenkins.getQueue().getBuildableItems();
-        assertThat("There is no buildable items in the queue. The job is not blocked", buildableItems.size(), equalTo(1));
+        assertThat(
+                "There is no buildable items in the queue. The job is not blocked", buildableItems.size(), equalTo(1));
         assertThat(buildableItems.get(0).task, instanceOf(ExecutorStepExecution.PlaceholderTask.class));
         assertThat("The task is not blocked", buildableItems.get(0).getCauseOfBlockage(), not(nullValue()));
     }
 
     @Nonnull
-    private QueueTaskFuture<WorkflowRun> runAsUser(WorkflowJob job, String username) throws InterruptedException, ExecutionException {
+    private QueueTaskFuture<WorkflowRun> runAsUser(WorkflowJob job, String username)
+            throws InterruptedException, ExecutionException {
         QueueTaskFuture<WorkflowRun> runFuture;
         SecurityContext old = ACL.impersonate((User.getById(username, true)).impersonate());
         try {
@@ -116,9 +113,9 @@ public class UserIDCauseRestrictionPipelineTest {
         return runFuture;
     }
 
-
     @Nonnull
-    private WorkflowRun runAsUserAndWaitForCompletion(WorkflowJob job, String username) throws InterruptedException, ExecutionException {
+    private WorkflowRun runAsUserAndWaitForCompletion(WorkflowJob job, String username)
+            throws InterruptedException, ExecutionException {
         // We have a test timeout, extra timeout is not needed
         return runAsUser(job, username).get();
     }
